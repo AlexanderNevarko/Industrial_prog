3c9dbff4860656d8a9d123b12c46eb64bf4a2488
Alexander <aleksandr.nevarko@frtk.ru>
2019-04-17 14:04:26 +0300

    Update utest_fizzbuzz.py
    Added unittests!

--- utest_fizzbuzz.py
+++ utest_fizzbuzz.py
@@ -3,5 +3,44 @@ import fizzbuzz
 
 
 class FizzbuzzTest(unittest.TestCase):
-    def test_1(self):
-        
+    def test_big_num(self):
+        seq = ['10000000000005000000000000000',
+                 '2000000000005000000000000001',
+                 '333333333333333333333333333333',
+                 '555555555555555505555555555555']
+        ans = ['fizzbuzz',
+                '2000000000005000000000000001',
+                'fizz',
+                'buzz']
+        self.assertListEqual(fizzbuzz.fizzbuzz(seq), ans)
+
+    def test_neg_num(self):
+        seq = ['-11110101010201102',
+               '89123123125',
+               '-15',
+               '-07645',
+               '-33333']
+        ans = ['-11110101010201102',
+               'buzz',
+               'fizzbuzz',
+               'buzz',
+               'fizz']
+        self.assertListEqual(fizzbuzz.fizzbuzz(seq), ans)
+
+    def test_bad_num(self):
+        seq = ['0000098',
+               '-9876545',
+               '000000000005',
+               '00000000000010000002',
+               '-000011115',
+               '00000033']
+        ans = ['0000098',
+               'buzz',
+               'buzz',
+               'fizz',
+               'fizzbuzz',
+               'fizz']
+        self.assertListEqual(fizzbuzz.fizzbuzz(seq), ans)
+
+if __name__ == '__main__':
+   unittest.main()
