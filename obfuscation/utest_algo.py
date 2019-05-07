f0e25435c35ba02cf69efcc9ea54772cb5a98540
Alexander <aleksandr.nevarko@frtk.ru>
2019-05-08 02:16:04 +0300

    travis for obf

--- obfuscation/utest_algo.py
+++ obfuscation/utest_algo.py
@@ -3,20 +3,22 @@ import unittest
 
 
 class AlgorythmTest(unittest.TestCase):
+    filename = 'obfuscation/textfile.txt'
+    
     def test_good1(self):
-        table = algo.prepare_table('textfile.txt')
+        table = algo.prepare_table(self.filename)
         self.assertEqual(algo.table_search(table, 1023, algo.hash_func, 'I'), True)
 
     def test_good2(self):
-        table = algo.prepare_table('textfile.txt')
+        table = algo.prepare_table(self.filename)
         self.assertEqual(algo.table_search(table, 1023, algo.hash_func, 'Gregor'), True)
 
     def test_bad1(self):
-        table = algo.prepare_table('textfile.txt')
+        table = algo.prepare_table(self.filename)
         self.assertEqual(algo.table_search(table, 1023, algo.hash_func, 'qwdasxqwd'), False)
 
     def test_bad2(self):
-        table = algo.prepare_table('textfile.txt')
+        table = algo.prepare_table(self.filename)
         self.assertEqual(algo.table_search(table, 1023, algo.hash_func, '__xasxk_ldkm'), False)
 
 
